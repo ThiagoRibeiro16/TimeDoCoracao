d301bc1aac97c8ca9b20d6ed73d6a6c3e671ea1d
ThiagoRibeiro16 <thiago.escola321@gmail.com>
2025-03-15 17:44:27 -0300

    add nome do time e coloca cores

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("         ");
+
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = (ConsoleColor)0;
+Console.WriteLine("São paulo");
+
+Console.BackgroundColor = (ConsoleColor)0;
+Console.WriteLine("         ");
+
+
+
+
+
+
+
+
+
